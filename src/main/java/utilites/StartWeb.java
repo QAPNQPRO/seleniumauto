d0ec3afe9c14fd6748c9858ed0558ad426559889
QAPNQPRO <97789939+QAPNQPRO@users.noreply.github.com>
2022-03-16 12:08:34 +0530

    Hello world

--- src/main/java/utilites/StartWeb.java
+++ src/main/java/utilites/StartWeb.java
@@ -13,8 +13,8 @@ public class StartWeb {
 		
 		
 		public WebDriver appStart() {
-			System.setProperty("webdriver.gecko.driver", "/home/tuhin/.jenkins/workspace/testseleniumscript/src/main/resources/geckodriver.exe");
-			driver = new FirefoxDriver();
+			System.setProperty("webdriver.chrome.driver", "/home/tuhin/.jenkins/workspace/testseleniumscript/src/main/resources/chromedriver/chromedriver.exe");
+			driver = new ChromeDriver();
 			driver.navigate().to("https://www.flipkart.com/");
 			driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 			driver.manage().window().maximize();
