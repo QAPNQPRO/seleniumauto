c62ec63338157fdeb9070ba3017a57735a17a0d9
QAPNQPRO <97789939+QAPNQPRO@users.noreply.github.com>
2022-03-16 12:05:17 +0530

    Update

--- src/main/java/utilites/StartWeb.java
+++ src/main/java/utilites/StartWeb.java
@@ -13,11 +13,11 @@ public class StartWeb {
 		
 		
 		public WebDriver appStart() {
-			System.setProperty("webdriver.gecko.driver", "/home/tuhin/.jenkins/workspace/testseleniumscript/src/main/resources/geckodriverlinux");
-			this.driver = new FirefoxDriver();
-			this.driver.navigate().to("https://www.flipkart.com/");
-			this.driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-			this.driver.manage().window().maximize();
+			System.setProperty("webdriver.gecko.driver", "/home/tuhin/.jenkins/workspace/testseleniumscript/src/main/resources/geckodriver.exe");
+			driver = new FirefoxDriver();
+			driver.navigate().to("https://www.flipkart.com/");
+			driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+			driver.manage().window().maximize();
 			return driver;
 		}
 		
